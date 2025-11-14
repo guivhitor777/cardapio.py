6edfe7cc3e7a8943faf003dabfca7a8d4fed312e
Muel777666 <agathacunha45@gmail.com>
2025-11-14 11:57:42 -0300

    salvo

---
+++ funcoes.py
@@ -0,0 +1,11 @@
+listacardapio = []
+def carregar_cardapio(listacardapio):
+    cardapio = [{
+"id": 1, "nome": "Hambúrguer", "preco": 12.5,
+"id": 2, "nome": "Pizza", "preco": 30,
+"id": 3, "nome": "Refrigerante", "preco": 5,
+}]
+
+def  exibir_carapio():
+    return carregar_cardapio
+
